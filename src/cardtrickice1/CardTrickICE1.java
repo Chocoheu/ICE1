c8ec6bf18d079b537533f430028a2fd5d11e8cb5
Chocoheu <phaminhh@sheridancollege.ca>
2022-06-13 22:24:22 +0700

    branch

--- src/cardtrickice1/CardTrickICE1.java
+++ src/cardtrickice1/CardTrickICE1.java
@@ -13,7 +13,6 @@ package cardtrickice1;
  * Minh Ha Pham 991647919
  */
 
-import java.util.Random;
 import java.util.Scanner;
 public class CardTrickICE1 
 {
@@ -23,39 +22,39 @@ public class CardTrickICE1
      */
     public static void main(String[] args) 
     {
-	Card luckyCard = new Card(7, "Hearts");
+//	Card luckyCard = new Card(7, "Hearts");
         Card[] magicHand = new Card[7]; //Array of object
+        System.out.println("THE HAND IS: ");
         
-        Random random = new Random();	
-	int x;
-        
-        Card c1 = new Card();
-        
-        for( int i=0;i<magicHand.length;i++)
+        for( int i = 0; i<magicHand.length; i++)
         {
-            x = random.nextInt(13)+1;
-            c1.setValue(x);//use a method to generate random *13
-            c1.setSuits(Card.SUITS[new Random().nextInt(Card.SUITS.length)]);//random method suit 
-            System.out.print(x + " of ");
-            System.out.println( Card.SUITS[new Random().nextInt(Card.SUITS.length)]);
+            Card c1 = new Card();
+            c1.setValue((int)(Math.random()*13));//use a method to generate random *13
+            c1.setSuits(Card.SUITS[(int)(Math.random()*4)]);//random method suit 
+            magicHand[i] = c1;
+            System.out.println(magicHand[i].getValue()+" of "+ magicHand[i].getSuits());
         }
         //step 2:take input 
-        System.out.println("Please choose the suit: \n 1: Hearts \n 2: Diamonds \n 3: Spades \n 4: Clubs");
         Scanner kb = new Scanner(System.in);
-        int cardSuit = kb.nextInt();
         System.out.println("Please choose the value (1 to 13)");
-        int cardNumber = kb.nextInt();
+        int number = kb.nextInt();
+        kb.nextLine();
+        
+        System.out.println("Please choose the suit: \n 1: Hearts \n 2: Diamonds \n 3: Spades \n 4: Clubs");
+        String s = kb.nextLine();
         
         //step 3: match with array 
 //        int value = c1.getValue();
 //        String suits = c1.getSuits();
         
-        if(c1.getValue() == cardNumber)
+        for(Card magicHand1: magicHand)
         {
-            System.out.println("Match.");
-        }else
-        {
-            System.out.println("Sorry, no match.");
+            if(magicHand1.getValue() == number && magicHand1.getSuits().equalsIgnoreCase(s))
+            {
+                System.out.println("Match");
+                return;
+            }
         }
+        System.out.println("Sorry, no match.");
     }
 }
